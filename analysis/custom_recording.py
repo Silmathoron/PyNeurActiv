c826a21a8a7bd5d4404e9c86f18c77f705a09475
Silmathoron <silmathoron@laposte.net>
2017-06-05 18:15:41 +0200

    added recorder

---
+++ analysis/custom_recording.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of the PyNeurActiv project, which aims at providing tools
+# to study and model the activity of neuronal cultures.
+# Copyright (C) 2017 SENeC Initiative
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+""" Tools to monitor the activity of neurons """
+
+import numpy as np
+
+import nngt
+from nngt.geometry import Shape
+
+
+__all__ = ["Recorder"]
+
+
+# --------------- #
+# Recorder class  #
+# --------------- #
+
+class Recorder:
+    
+    '''
+    Intelligent recording device which allows to monitor localized areas.
+    '''
+    pass
