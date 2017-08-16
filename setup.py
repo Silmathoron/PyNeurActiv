79de58f6cbec38a44d69f28e1b4236b5cdf413c2
Silmathoron <Silmathoron@users.noreply.github.com>
2017-08-16 15:14:49 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+setup(
+        name = 'PyNeurActiv',
+        version = '0.1.0',
+        description = 'Python tools and theoretical models for neuronal activity analysis',
+        package_dir = {'': '.'},
+        packages = find_packages('.'),
+
+        # Requirements
+        install_requires = ['numpy', 'scipy>=0.11', 'matplotlib'],
+        extras_require = {
+            'nngt': 'git+https://github.com/Silmathoron/NNGT.git',
+            'pandas': 'pandas'
+        },
+
+        # Metadata
+        url = 'https://github.com/SENeC-Initiative/PyNeurActiv',
+        author = 'Tanguy Fardet',
+        author_email = 'tanguy.fardet@univ-paris-diderot.fr',
+        license = 'GPL3',
+        keywords = 'neural activity analysis simulation'
+)
